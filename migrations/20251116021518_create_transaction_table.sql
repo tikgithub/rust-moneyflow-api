4900e4e9b19e8c10a4d1e273187dbf99ed8a2362
TikAlienware <vadsalasack.vs@gmail.com>
2025-11-16 09:19:35 +0700

    update

--- migrations/20251116021518_create_transaction_table.sql
+++ migrations/20251116021518_create_transaction_table.sql
@@ -1,11 +1,15 @@
-CREATE TABLE categories (
+-- Add migration script here
+CREATE TABLE transaction (
     id SERIAL4 not null,
     user_id integer not null,
-    name varchar(255) not null,
+    category_id integer not null,
+    type varchar(255) not null,
+    amount DECIMAL(12, 2) DEFAULT 0.00,
+    memo text,
     description text,
-    balance DECIMAL(12, 2) DEFAULT 0.00,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
     FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
     PRIMARY KEY (id)
 );
